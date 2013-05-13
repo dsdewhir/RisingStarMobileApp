18ebc5990f793429625979b770aafda2c1a50133
Jesse Briggs <j@pc>
2013-05-13 16:41:56 -0400

    Add player.js

---
+++ www/js/player.js
@@ -0,0 +1,15 @@
+function newPlayer(player_name) {
+	Q = "INSERT INTO Player (name) VALUES '" + player_name + "')";
+	query(Q);
+}
+
+function createPlayerTable() {
+	Q = "CREATE TABLE IF NOT EXISTS Player(id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL)";
+	query(Q);
+}
+
+if (reset==true) {
+	query("DROP TABLE Player");
+}
+
+createPlayerTable();
