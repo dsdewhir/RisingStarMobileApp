47ca07b4d561b5c1ad70168d02a6c6660b24bc4d
Jesse Briggs <j@pc>
2013-05-15 14:47:25 -0400

    Make adding games work. Fix the form, it had silly stuff

--- www/js/game.js
+++ www/js/game.js
@@ -2,7 +2,7 @@ function loadGames(team_id) {
 	$("#gamelist").html("");
 	Q = "SELECT * FROM Game WHERE team_id = '" + team_id + "';";
 	act_on_results(Q, function(row) { 
-		opponent = (row.at_home == 0) ? "@" : "";
+		opponent = (row.at_home == 0) ? "Away @" : "Home vs";
 		opponent += row.opponent;
 		$('#gamelist').append('<li class="forward"><a href="#game">' + opponent + ' ' + row.date + '</a></li>'); 
 	});
@@ -12,7 +12,7 @@ function loadGames(team_id) {
 function newGame(team_id, opponent, sport, date, at_home) {
 	Q = "INSERT INTO Game (team_id, opponent, sport, date, at_home) VALUES ('" + team_id + "', '" + opponent + "', '" + sport + "', '2013-05-15', " + at_home + ")";
 	query(Q);
-	//log("Created new team: " + team_name);
+	log("Created new team: " + team_name);
 }
 
 function createGameTable() {
