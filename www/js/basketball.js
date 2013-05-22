c6f937efd346518e7db438df400b5fb2108632db
Jesse Briggs <jesse@threetwelvecreative.com>
2013-05-22 17:47:39 -0400

    Add basketball stat show functions

--- www/js/basketball.js
+++ www/js/basketball.js
@@ -10,6 +10,28 @@ function loadBasketballScores(game_id, inning) {
 	}, function(){} );
 }
 
+function resetBasketballStats() {
+	$("#show-basketball #stat-container span").text(0);	
+}
+
+function loadBasketballTeamStats(team_id) {
+	Q = "SELECT * FROM Game WHERE team_id = " + team_id; //Get all the games, so we can look them up by id
+	act_on_results(Q, function(row) {
+		log(row.opponent);
+		loadBasketballStats(row.id);
+	});
+}
+
+function loadBasketballStats(game_id) { //fill values in the stats show
+	resetBasketballStats();
+	//$("#show-baseball #stat-container #stat_display_atbats span")	
+	Q = "SELECT * FROM Score WHERE game_id = " + game_id;
+	act_on_results(Q, function(row) {
+		html_row = $("#show-basketball #stat-container #stat_display_" + row.field + " span");
+		html_row.text(parseInt(html_row.text()) + row.amt);
+	});
+}
+
 /* EVENTS */
 bbstats = ["fieldgoal", "threepointer", "freethrow", "rebound", "steal", "block", "turnover"];
 for (var i = 0; i < bbstats.length; i++ ) {
