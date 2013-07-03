784610b17c016cf9ce554fd8e8ea62d27cbcd981
Jesse Briggs <j@pc>
2013-07-03 16:02:29 -0400

    Include dependencies. try a simple datepicker on new game form

--- www/js/events.js
+++ www/js/events.js
@@ -89,6 +89,10 @@ $(document).ready(function() {
 	
 	$(".saveclear").click(function() { $("input[type=text]").val(""); });
 	$("#player_photo").click(function() { getPicture(); });
+
+	$("#gamedate").click(function() {
+		openGameDate();
+	});
 });
 
 function onCameraError(message) {
@@ -97,4 +101,36 @@ function onCameraError(message) {
 
 function onCameraSuccess(imageURI) {
 	$("#imagebox").attr("src", imageURI);
+}
+
+function openGameDate() {
+	var now = new Date();
+	var days = { };
+	var years = { };
+	var months = { 1: 'Gen', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec' };
+	
+	for( var i = 1; i < 32; i += 1 ) {
+		days[i] = i;
+	}
+
+	for( i = now.getFullYear()-100; i < now.getFullYear()+1; i += 1 ) {
+		years[i] = i;
+	}
+
+	SpinningWheel.addSlot(years, 'right', 1999);
+	SpinningWheel.addSlot(months, '', 4);
+	SpinningWheel.addSlot(days, 'right', 12);
+	
+	SpinningWheel.setCancelAction(cancel);
+	SpinningWheel.setDoneAction(done);
+	
+	SpinningWheel.open();
+}
+
+function done() {
+	var results = SpinningWheel.getSelectedValues();
+	alert('values:' + results.values.join(', ') + ' - keys: ' + results.keys.join(', '));
+}
+
+function cancel() {
 }
