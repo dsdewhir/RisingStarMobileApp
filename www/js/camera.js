893b7aac409b51543533e401277dc69ba118f8f0
Jesse Briggs <jesse@threetwelvecreative.com>
2013-07-09 10:40:24 -0400

    Change alerts back to logs to use with weinre

--- www/js/camera.js
+++ www/js/camera.js
@@ -1,19 +1,19 @@
 function getPicture() {
-	alert("Getting picture");
+	log("Getting picture");
 	navigator.camera.getPicture(onCameraSuccess, onCameraFail, { quality: 30,
 	    destinationType: Camera.DestinationType.FILE_URI,
 	    sourceType : Camera.PictureSourceType.PHOTOLIBRARY,
 	 });
-	 alert("Done");
+	 log("Done");
 } 
 
 function onCameraSuccess(imageData) {
-	alert("Camera success");
+	log("Camera success");
     var image = $("#myImage");
     image.attr("src", imageData);
-    alert("img set to " + imageData);
+    log("img set to " + imageData);
 }
 
 function onCameraFail(message) {
-    alert('Failed because: ' + message);
+    log('Failed because: ' + message);
 }
