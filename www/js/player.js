caf41605fcbbabadb6ca5e8e18bbbc37cf3a0d3c
Jesse Briggs <jesse@threetwelvecreative.com>
2013-07-12 16:54:25 -0400

    Remove NOT NULL requirement on player.photo

--- www/js/player.js
+++ www/js/player.js
@@ -121,7 +121,7 @@ function Players () {
 
 function playerInitialize() {
 	function createPlayerTable() {
-		var Q = "CREATE TABLE IF NOT EXISTS Player(id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, photo TEXT NOT NULL)";
+		var Q = "CREATE TABLE IF NOT EXISTS Player(id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, photo TEXT)";
 		query(Q);
 	}
 	
